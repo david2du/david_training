c3636908e2b9400d798d3a3f6230c5f98d2b638e
david2du <david2du@163.com>
2022-05-02 16:24:31 +0800

    sync with mac

--- test/test/test-ublb.cpp
+++ test/test/test-ublb.cpp
@@ -3,8 +3,13 @@ using namespace std;
 
 int main()
 {
+<<<<<<< HEAD:gao/2020/Unit7/homework/hm-A.cpp
+	cout << "0" << endl;
+	return 0;
+=======
     vector<int> v;
     lower_bound(v.begin(), v.end(), 0);
 
     return 0;
+>>>>>>> move test:old_test/test/test-ublb.cpp
 }
